ed743428f681705472466c5f03f6152942fc6144
kroos_chen <sean830314@gmail.com>
2021-02-03 17:31:19 +0800

    Add environment initialization script

---
+++ init.sh
@@ -0,0 +1,7 @@
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+python3 -m spacy download en_core_web_sm
+python3 -m nltk.downloader maxent_ne_chunker
+python3 -m nltk.downloader words
+curl -O -L http://nlp.stanford.edu/software/stanford-corenlp-latest.zip
+unzip stanford-corenlp-latest.zip
